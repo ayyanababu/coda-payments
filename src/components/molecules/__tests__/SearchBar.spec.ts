15a5fb0e83789c1f1f42e14ee98d114d2c65ca4e
Raja ayyanababu <ayyanababu@gmail.com>
2023-12-22 15:25:14 +0530

    added test cases for searchBar

---
+++ src/components/molecules/__tests__/SearchBar.spec.ts
@@ -0,0 +1,53 @@
+import { describe, it, expect, beforeEach } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { createStore } from 'vuex'
+import SearchBar from '@/components/molecules/SearchBar.vue'
+
+describe('SearchBar.vue', () => {
+
+  let actions
+  let store
+
+  beforeEach(() => {
+    actions = {
+      'home/updateSearchQuery': function() {
+        this.called = true
+      }
+    }
+    store = createStore({
+      actions
+    })
+  })
+
+
+  it('renders a search input field', () => {
+    const wrapper = mount(SearchBar)
+    expect(wrapper.find('input[type="text"]').exists()).toBe(true)
+  })
+
+  it('renders a search icon', () => {
+    const wrapper = mount(SearchBar)
+    expect(wrapper.find('img').exists()).toBe(true)
+  })
+
+  it('clears the search query when input is cleared', async () => {
+    const wrapper = mount(SearchBar)
+    const input = wrapper.find('input[type="text"]')
+  
+    await input.setValue('test query')
+    expect(wrapper.vm.searchQuery).toBe('test query')
+  
+    await input.setValue('')
+    expect(wrapper.vm.searchQuery).toBe('')
+  })
+
+
+  it('updates the search query when input is entered', async () => {
+    const wrapper = mount(SearchBar)
+    const input = wrapper.find('input[type="text"]')
+
+    await input.setValue('test query')
+    expect(wrapper.vm.searchQuery).toBe('test query')
+  })
+
+})
